20e49e0272db7c57e4c16ae6f2e24c5dfe47b30e
= <=>
2025-01-15 19:04:02 +0530

    db fix, favicon added

--- server.js
+++ server.js
@@ -1,37 +1,46 @@
-// const http = require('http');
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const dotenv = require('dotenv');
-// const connectDB = require('./config/db');
-// const authRoutes = require('./routes/authRoutes');
-// const blogRoutes = require('./routes/blogRoutes');
-// const verifyToken = require('./middlewares/verifyToken');
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+const verifyToken = require('./middlewares/verifyToken');
 
-// dotenv.config();
+dotenv.config();
 
-// const app = express();
+const app = express();
+const url = process.env.URL || 'http://localhost:3004';
 
-// // Define CORS options
-// const corsOptions = {
-//   origin: process.env.CLIENT_URL || 'http://localhost:3004',  // The frontend URL
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed methods
-//   credentials: true,  // Allow cookies to be sent
-// };
+// Define CORS options
+const corsOptions = {
+  origin: process.env.CLIENT_URL || 'http://localhost:3004',  // The frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed methods
+  credentials: true,  // Allow cookies to be sent
+};
 
-// // Use CORS middleware with custom options
-// app.use(cors(corsOptions));
+// Use CORS middleware with custom options
+app.use(cors(corsOptions));
 
-// // Middleware
-// app.use(bodyParser.json());
+// Middleware
+app.use(bodyParser.json());
 
-// // Connect to Database
-// connectDB();
+// Connect to Database
+connectDB();
 
-// // Routes
-// app.get('/', (req, res) => {
-//   res.send('Welcome to the server!');
-// });
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the server!');
+});
 
-// app.use('/api/auth', authRoutes);
-// app.use('/api/blogs', blogRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/blogs', blogRoutes);
+
+// Create an HTTP server
+const server = http.createServer(app);
+
+// Start Server
+server.listen(url, () => {
+  console.log(`Server running on ${url}`);
+});
